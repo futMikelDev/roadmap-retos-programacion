809263509d18544e971f3830b247452430bda4ad
futMikelDev <125033993+futMikelDev@users.noreply.github.com>
2024-04-27 19:48:57 +0200

    Create futMikelDev.js
    Primer ejercicio del Roadmap de los Retos-de-programación de Mouredev

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/#00/javascript/futMikelDev.js
@@ -0,0 +1,19 @@
+// https://javascript.org
+
+// Comentario de una línea
+
+/*
+Comentario de
+varias
+líneas
+*/
+
+let variableDeJavaScript
+const constanteDeJavasCript
+
+let unNumero = 100
+let float = 3.14
+let boolean = true
+let string = 'Buenas tardes Mouredev 😊'
+
+console.log('Hola mundo')
